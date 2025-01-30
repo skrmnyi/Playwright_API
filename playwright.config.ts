30efb1685a740945fa7b8758349994519dc34dc9
Ansko <carhatt.xm@gmail.com>
2025-01-30 03:09:31 +0200

    save

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './tests',
  testMatch: '*.spec.ts',
@@ -39,55 +29,27 @@ export default defineConfig({
   },
 
   /* Configure projects for major browsers */
-  projects: [
-    {name : 'setup', testMatch: 'auth.setup.ts'   /* проект буде ранити тільки auth.setup.ts тест  */
-    
-
-    }, 
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'], storageState: './auth/user.json'},  /* браузер буде ранитись з  сторейдж стейтом*/
-      dependencies: ['setup'] // dependencies означає що перед тим як браузер запуститься потрібно виконати setup дію. тобто пройти авторизацію
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'], storageState: './auth/user.json' },
-      dependencies: ['setup']
-      
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'], storageState: './auth/user.json' },
-      dependencies: ['setup']
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
-});
+  /* Configure projects for major browsers */
+projects: [
+  {
+    name: 'setup',
+    testMatch: 'auth.setup.ts',
+  },
+  {
+    name: 'articleSetup',
+    testMatch: 'newArticleSetup.spec.ts',
+    dependencies: ['setup'],
+  },
+  {
+    name: 'regression',
+    use: { ...devices['Desktop Chrome'], storageState: '.auth/user.json' },
+    dependencies: ['setup'],
+  },
+  {
+    name: 'likeCounter',
+    testMatch: 'likesCounter.spec.ts',
+    use: { ...devices['Desktop Chrome'], storageState: '.auth/user.json' },
+    dependencies: ['articleSetup'],
+  },
+]
+})
